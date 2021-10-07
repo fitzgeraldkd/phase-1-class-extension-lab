a2e8a50c8f8d7aeab6513070e1edb1781c81a1e3
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-10-07 12:50:56 -0400

    commit tree 46b2a1fc5226746a9d770b793ee17f309aa61480

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        return this.sides.reduce((prev, current) => prev + current);
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.countSides !== 3) return false;
+        const sidesSorted = [...this.sides].sort((first, second) => {
+            if (first < second) return -1;
+            if (first > second) return 1;
+            return 0;
+        });
+        if (sidesSorted[0] + sidesSorted[1] > sidesSorted[2]) return true;
+        return false;
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        if (this.countSides !== 4) return false;
+        if (this.sides.every(side => side === this.sides[0])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    get area() {
+        return this.sides[0] ** 2;
+    }
+}
